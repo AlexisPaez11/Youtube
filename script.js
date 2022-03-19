d62adf1ee7c0802cf62a570fc77be3ba56d5e73c
Alexis Paez <77308122+AlexisPaez11@users.noreply.github.com>
2022-03-18 23:24:26 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+const body = document.querySelector("body"),
+    sidebar = body.querySelector(".sidebar"),
+    sidebarClose = body.querySelector(".close"),
+    main = body.querySelector("main"),
+    menuButton = body.querySelector(".menu-button-icon"),
+    modeSwitch = body.querySelector(".toggle-switch"),
+    modeText = body.querySelector(".mode-text"),
+    opacity = body.querySelector(".body-container");
+
+    menuButton.addEventListener("click", () => {
+        sidebar.classList.toggle("close");
+    })
+
+    modeSwitch.addEventListener("click", () => {
+        body.classList.toggle("light");
+
+        if(body.classList.contains("light")){
+            modeText.innerText = "Light Mode"
+        }
+        else{
+            modeText.innerText = "Dark Mode"
+        }
+    })
